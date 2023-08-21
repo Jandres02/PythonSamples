66bc7c9d693331b79c7a86c8791b1fdeea4ff2e0
Jandres02 <avjohan@uninorte.edu.co>
2023-08-21 14:27:06 -0500

    new functions

---
+++ FGO.py
@@ -0,0 +1,5 @@
+def fgo(n):
+    fz = (n(n+1))/(1-n)**2
+    return fz
+
+fgo(1)
--- messages.py
+++ messages.py
@@ -1,14 +1,27 @@
 import pyautogui as pt
+import pyperclip
 import time
 
-limit = input("Number of messages: ")
-message = input("Message: ")
-i = 0
+# Línea desde la cual comenzar a leer (índice basado en 0)
+start_line = 251  # Cambia esto al número de línea deseado
+stop_line = 265
+  # Cambia esto al número de línea donde deseas detener el proceso
 
+# Tiempo de espera inicial
 time.sleep(3)
 
+# Abrir el archivo de texto con la lista de correos electrónicos
+with open('correos.txt', 'r') as file:
+    emails = file.readlines()
 
-while i < int(limit):
-    pt.typewrite(message)
+# Recorrer la lista de correos electrónicos y enviar los mensajes
+for i in range(start_line - 1, len(emails)):
+    email = emails[i].strip() + "@uninorte.edu.co"  # Agregar el dominio
+    pyperclip.copy(email)  # Copiar el correo electrónico al portapapeles
+    pt.hotkey('ctrl', 'v')  # Pegar el correo desde el portapapeles
+    time.sleep(2)  # Agregar un tiempo de espera antes de pegar el siguiente correo
     pt.press("enter")
-    i += 1
+    time.sleep(2)  # Agregar un tiempo de espera antes de pegar el siguiente correo
+    
+    if i == stop_line - 1:
+        break  # Salir del bucle cuando alcance la línea deseada
